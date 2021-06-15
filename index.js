ceab266a7c0d7587d8b05e48cc18e82665c9a5e4
JaoVizu <joaovizu2015@gmail.com>
2021-06-15 15:55:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function uniqueDigit(value){
+  if(String(value).length === 1){
+    console.log(`O digito único é ${value}`)
+  }else{
+    value = sumOfNumbers(value)
+    uniqueDigit(value)
+  }
+}
+
+function sumOfNumbers(value){
+  const splittedNumbers = String(value).split('')
+  let sum = 0
+  splittedNumbers.forEach(number => sum += Number(number))
+  return sum
+}
+
+function concatNumbers(n ,k) {
+  let p = ''
+  for(let i = k; i > 0; i--) p += n
+  uniqueDigit(p)
+}
+
+uniqueDigit(9875)
+concatNumbers(9875, 4)
